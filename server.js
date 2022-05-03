b440777476b584b1c64f763b6967b37bf335bddb
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-03 16:47:33 -0400

    added mysql support

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require('mysql2');
 
 // Sets the localhost port
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,17 @@ const app = express();
 // Express Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connects to the mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'liquiddeath2022',
+        database: 'election'
+    },
+    console.log('Connected to the Election Database!')
+);
 
 // GET test route
 app.get("/", (req, res) => {
