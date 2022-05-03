8a7f09cedd978fe0d9929f1ba37e5b52b5b96b49
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-03 17:02:29 -0400

    tested base sql commands

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // Sets the localhost port
 const PORT = process.env.PORT || 3001;
@@ -11,13 +11,13 @@ app.use(express.json());
 
 // Connects to the mysql database
 const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'liquiddeath2022',
-        database: 'election'
-    },
-    console.log('Connected to the Election Database!')
+  {
+    host: "localhost",
+    user: "root",
+    password: "liquiddeath2022",
+    database: "election",
+  },
+  console.log("Connected to the Election Database!")
 );
 
 // GET test route
@@ -28,9 +28,38 @@ app.get("/", (req, res) => {
 });
 
 // Returns data for the candidates table
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-}); 
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     // Logs the rows in the table
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
